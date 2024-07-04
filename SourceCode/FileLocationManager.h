4350cbd8d9097928ac0f64cce3fdb30df5453541
loi777 <35152365+loi777@users.noreply.github.com>
2024-07-04 14:44:20 -0300

    Update FileLocationManager.h
    added H lib

--- SourceCode/FileLocationManager.h
+++ SourceCode/FileLocationManager.h
@@ -1,1 +1,17 @@
+// Get the current directory from the therminal window
+void FM_getDirAtTerminal();
 
+// Print the Directory path that the code is in at the moment
+void FM_printDir();
+
+// Print the Video files in the current folder path
+// video files valid: .mp4 | .webm
+void FM_printVideoFiles();
+
+// Prints out the Folders directly acessible
+// and allows the user to change dir with one of the listed folders.
+void FM_goDownDir();
+
+// Go Up a Dir by removing the current folder from the saved Directory Path.
+// if you go up all folders it will return a single '/'
+void FM_goUpDir();
